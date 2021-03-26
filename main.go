9aee812be983e681a9cda59dc42d4fd66eaf0216
Anonna Ferdaus <44522472+AnonnaGH@users.noreply.github.com>
2021-03-26 23:55:55 +0600

    Update main.go

--- main.go
+++ main.go
@@ -5,5 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 	fmt.Println("I an Anonna")
-	fmt.Println("I an Anonna Ferdaus Tithi")
+	fmt.Println("I am Anonna Ferdaus Tithi")
+	fmt.Println("I am a student")
+}
 }
